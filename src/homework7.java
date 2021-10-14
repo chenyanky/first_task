d4334bf694368cd0988b53cc2a515f515e5ceb00
chenyanky <1874835542@qq.com>
2021-10-14 11:13:09 +0800

    task2-1

---
+++ src/homework7.java
@@ -0,0 +1,19 @@
+
+public class homework7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      double radius;//declare radius
+      double area;//declare area
+      
+      //Assign a radius
+      radius=20;//radius is now 20
+      
+      //Compute area
+      area=radius*radius*3.14159;
+      
+      //Display results
+      System.out.println("The area for the circle of radius"+radius+"is"+area);
+	}
+
+}
